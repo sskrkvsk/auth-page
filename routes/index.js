c1015344515f9449e969661793d780568b167cca
ssk <s99ruy@gmail.com>
2024-01-19 04:46:20 +0100

    finish

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-// var db = require('../db');
+var db = require('../db');
+const { name } = require('ejs');
 
 
 // Home route
@@ -8,7 +9,20 @@ router.get('/', function(req, res, next) {
   if (!req.user) {
     res.render('login', { title: 'Ass' });
   } else {
-    res.render('home', { user: req.user });
+    const id = req.user.id;
+    db.get('SELECT name FROM users WHERE id = ?', [req.user.id], (err, row) => {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+      if (row) {
+        var userName = row.name;
+        res.render('home', { user: req.user, userName: userName });
+      } else {
+        console.log('No matching record found.');
+        res.render('home', { user: req.user});
+      }
+    });    
   }
 });
 
